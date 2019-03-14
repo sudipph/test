773d1cb68f02fbee61c0790a6a4f0e676aafb4f4
sudip321 <sudip.podder@capitalnumbers.com>
2019-03-14 19:24:56 +0530

    Revert "branch commit"
    This reverts commit 3f09814fcaf894a4e6456641ddb8853dae0dc889.

--- index.php
+++ index.php
@@ -8,20 +8,3 @@ test git changed
 test git changed
 test git changed
 test git changed
-
-git diff
-git diff
-git diff
-git diff
-git diff
-git diff
-git diff
-
-git new branch
-git new branch
-git new branch
-git new branch
-git new branch
-git new branch
-git new branch
-git new branch
